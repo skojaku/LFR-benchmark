f8a90a4d15c9da96151d2c07023fc13c6c89395b
Sadamori Kojaku <skojaku@iu.edu>
2023-06-15 12:17:54 -0400

    add binary networks

---
+++ setup.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+# @Author: Sadamori Kojaku
+# @Date:   2023-06-15 12:16:51
+# @Last Modified by:   Sadamori Kojaku
+# @Last Modified time: 2023-06-15 12:17:24
+import sys
+import subprocess
+
+from setuptools import setup
+from setuptools.command.build_ext import build_ext
+from setuptools.command.build_py import build_py
+from setuptools import find_packages, setup, Extension
+
+
+class Build(build_ext):
+    """Customized setuptools build command - builds protos on build."""
+
+    def run(self):
+        protoc_command = ["make"]
+        if subprocess.call(protoc_command) != 0:
+            sys.exit(-1)
+        super().run()
+
+
+class BuildPy(build_py):
+    """Customized setuptools build command - builds protos on build."""
+
+    def run(self):
+        protoc_command = ["make"]
+        if subprocess.call(protoc_command) != 0:
+            sys.exit(-1)
+        super().run()
+
+
+setup(
+    name="lfrbenchmark",
+    version="0.0",
+    description="LFR benchmark for networks",
+    packages=["lfrbenchmark"],
+    has_ext_modules=lambda: True,
+    cmdclass={
+        "build_ext": Build,
+    },
+)
---
+++ src/README.md
@@ -0,0 +1,19 @@
+The authors generously allow me to keep the original code in my repository. If you use this code, please cite 
+```
+@article{PhysRevE.78.046110,
+  title = {Benchmark graphs for testing community detection algorithms},
+  author = {Lancichinetti, Andrea and Fortunato, Santo and Radicchi, Filippo},
+  journal = {Phys. Rev. E},
+  volume = {78},
+  issue = {4},
+  pages = {046110},
+  numpages = {5},
+  year = {2008},
+  month = {Oct},
+  publisher = {American Physical Society},
+  doi = {10.1103/PhysRevE.78.046110},
+  url = {https://link.aps.org/doi/10.1103/PhysRevE.78.046110}
+}
+
+```
+
